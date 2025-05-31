f9ec718798bcc6e8e080707270aa2f2d9b17dce4
Nicolas Feng <nicolasfeng597@gmail.com>
2025-05-31 15:35:41 -0400

    misc for scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Scheduler {
@@ -16,7 +17,7 @@ public class Scheduler {
         // find all combinations of courses with courseCount items
         // Helper returns a list of course arrays : a list of all possible 5
         // courses, not considering overlaps, no repeats
-
+        
         // rewrite each combination into sections of each course
 
         // find all combinations of timeslots for each combination of sections
@@ -24,7 +25,7 @@ public class Scheduler {
         scanner.close();
     }
 
-    public static List<List<Section>> findPossibleSchedules(Course[] schedule) {
-        return null;
+    public static List<Course[]> combinations() {
+        return new ArrayList<Course[]>();
     }
 }
